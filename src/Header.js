5a8a934f75b464f40c9f3f041c1cc4b0782eb8c6
Martin Tsan <martintsan@pm.me>
2023-08-16 10:37:59 +0000

    feat: Added Header component to show ganme name and description

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <>
+      <h1>Numbered Nexus</h1>
+      <p>
+        Dive into the world of Numbered Nexus, a thrilling puzzle challenge that
+        beckons players to restore order to a chaotic 3x3 grid of tiles. Using
+        only the arrow keys, maneuver the numbered tiles, aiming to align them
+        in a continuous sequence. The final quest? To position the elusive empty
+        slot at the grid's bottom-right corner. With the clock ticking,
+        precision and strategy are your best allies. Can you establish the
+        perfect nexus of numbers?
+      </p>
+    </>
+  );
+};
+
+export default Header;
